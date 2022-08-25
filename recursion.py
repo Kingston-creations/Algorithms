dc97ff56b16d2cc56b97b5969e01a56e8df9aa0c
Tharshan <63536736+Kingston-creations@users.noreply.github.com>
2022-08-25 09:54:39 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,21 +1,14 @@
 def coinSum(coins, sum):
-    # Stop condition.
     if sum == 0:
         return [[]]
-
-    # Recursive step
     solutions = []
     for i, coin in enumerate(coins):
         if coin <= sum:
             lst = coinSum(coins[i:], sum - coin)
             solutions.extend([[coin] + l for l in lst])
     return solutions
-
-
 coins=list(map(int,input().split()))
-
 sum=int(input())
-
 coinSum(coins, sum)
 l=coinSum(coins, sum)
 l.sort(key=len)
